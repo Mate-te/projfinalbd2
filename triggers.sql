ce9cabd526d4388817a3b91b2c758458cc99059a
Mate-te <80487722+Mate-te@users.noreply.github.com>
2024-06-29 15:49:44 -0300

    Add files via upload

---
+++ triggers.sql
@@ -0,0 +1,62 @@
+-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TRIGGERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+CREATE OR REPLACE FUNCTION alteraDiponivelLivro()
+RETURNS TRIGGER AS $$
+DECLARE
+	livroid int;
+	clienteid int;
+BEGIN
+	select liv,cli
+	into livroid, clienteid
+    from Emprestimo
+    where id_emp = NEW.id_emp; 
+
+
+    UPDATE Livro
+	SET qnt_emprestimo = qnt_emprestimo + 1
+	WHERE id_livro = livroid;
+
+	UPDATE Cliente
+	SET Numero_emp = Numero_emp + 1
+	WHERE id_cliente = clienteid;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_alteraDiponivelLivro
+AFTER INSERT OR UPDATE ON Emprestimo
+FOR EACH ROW
+EXECUTE FUNCTION alteraDiponivelLivro();
+
+
+CREATE OR REPLACE FUNCTION validaCliente()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.nome := LOWER(NEW.nome);
+	IF NEW.data_reg >= CURRENT_DATE THEN
+        RAISE EXCEPTION 'A data de registro (%), deve ser anterior a data atual', NEW.data_reg;
+    END IF;
+	NEW.CPF := regexp_replace(NEW.CPF, '(\d{3})(\d{3})(\d{3})(\d{2})', '\1.\2.\3-\4');
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_validaCliente
+BEFORE INSERT OR UPDATE ON Funcionario
+FOR EACH ROW
+EXECUTE FUNCTION validaCliente();
+
+CREATE OR REPLACE FUNCTION validaCliente()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.Titulo := LOWER(NEW.Titulo);
+    NEW.autorL := LOWER(NEW.autorL);
+	NEW.ISBN := regexp_replace(NEW.ISBN, '(\d{3})(\d{1})(\d{2})(\d{6})(\d{1})', '\1-\2-\3-\4-\5');
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_validaLivro
+BEFORE INSERT OR UPDATE ON Livro
+FOR EACH ROW
+EXECUTE FUNCTION validaLivro();
+
