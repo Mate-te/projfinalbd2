caf30ae90bffe2db458c1bef5941dde7095e345f
GuilhermePedron <86671780+GuilhermePedron@users.noreply.github.com>
2024-07-01 20:25:23 -0300

    Create indices.sql

---
+++ indices.sql
@@ -0,0 +1,21 @@
+create index idx_tituloLivro on livro(titulo);
+
+create INDEX idx_livroautor on Livro(autorL);
+
+create INDEX idx_emprestimoinicio on Emprestimo(inicio);
+create INDEX idx_emprestimofim on Emprestimo(fim);
+
+
+explain analyze SELECTselect l.*
+from Livro l
+join Autor a on l.autorL = a.id_autor
+where a.Nome like 'A%';
+
+explain analyze select id_livro, isbn, categoria,id_autor, nome
+from livro l
+join autor a on a.id_autor = l.autorL
+where titulo = 'man between, the'
+
+explain analyze select * 
+from emprestimo 
+where inicio > '10-02-2023'
